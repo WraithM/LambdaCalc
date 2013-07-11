af710dbeb176ecdf40411e02ad960f6aad40b7c3
Matthew Wraith <wraithm@gmail.com>
2013-07-11 21:53:08 +0000

    Made a main for the language

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,21 @@
 module Main where
 
+import Text.Parsec.String (parseFromFile)
+
 import Parser
 import Eval
 
+parseFile :: FilePath -> IO [Message]
+parseFile filename = do
+    result <- parseFromFile parseAssignments filename
+    case result of
+        Left err -> do
+            print err
+            return []
+        Right as -> return as
+
+main :: IO ()
+main = do
+    as <- parseFile "test.l"
+    forM_ as $ \a ->
+        print a
